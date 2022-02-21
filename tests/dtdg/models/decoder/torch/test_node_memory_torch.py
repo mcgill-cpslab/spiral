047f51ac98ab42a3fec4671b40d32c620d62e7d0
CooKey-Monster <78682700+CooKey-Monster@users.noreply.github.com>
2022-02-20 17:27:08 -0800

    add test (#28)
    * add test
    
    * Changing comments on test_node_memory_torch.py and test_node_memory_tf.py

--- tests/dtdg/models/decoder/torch/test_node_memory_torch.py
+++ tests/dtdg/models/decoder/torch/test_node_memory_torch.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 # flake8: noqa
-"""Tests `nineturn.dtdg.models.decoder.torch.sequentialDecoder.rnnFamily` package."""
+"""Tests `nineturn.dtdg.models.decoder.torch.sequentialDecoder.implicitTimeModels` package."""
 import torch
 from tests.core.common_functions import *
 
 
-def test_ogb_dataset_torch():
+def test_node_memory_torch():
+    """Test NodeMemory"""
     clear_background()
-    """Test that citation graph could support different backend."""
     from nineturn.core.config import set_backend
     from nineturn.core.backends import PYTORCH
     set_backend(PYTORCH)
-    from nineturn.dtdg.models.decoder.torch.sequentialDecoder.rnnFamily import NodeMemory
+    from nineturn.dtdg.models.decoder.torch.sequentialDecoder.implicitTimeModels import NodeMemory
 
     n_nodes = 5
     hidden_d = 2
@@ -23,6 +23,6 @@ def test_ogb_dataset_torch():
     nodes_to_change = [2,3]
     new_memory = torch.randn(n_layers, 2, hidden_d)
     this_memory.update_memory(new_memory, nodes_to_change)
-    assert torch.equal(this_memory.get_memory(nodes_to_change, "cpu"), new_memory)
+    assert torch.equal(this_memory.get_memory(nodes_to_change), new_memory)
 
 
