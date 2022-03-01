decc7cdeaf8d3c99ee1a8d4d5ad681250dd30389
Yuecai <yuecai.zhu@bell.ca>
2022-02-28 20:17:17 -0500

    sync to run in GCP

--- nineturn/dtdg/models/decoder/tf/sequentialDecoder/explicitTimeDecoder.py
+++ nineturn/dtdg/models/decoder/tf/sequentialDecoder/explicitTimeDecoder.py
@@ -20,8 +20,8 @@ import tensorflow as tf
 from tensorflow import Tensor
 from tensorflow.keras.layers import MultiHeadAttention
 from nineturn.dtdg.models.decoder.tf.sequentialDecoder.baseModel import SlidingWindowFamily
-from nineturn.core.layers.time2Vec import Time2Vec
-from nineturn.core.layers.tsa import TSA
+from nineturn.core.layers import Time2Vec
+from nineturn.core.layers import TSA
 from nineturn.dtdg.models.decoder.tf.simpleDecoder import SimpleDecoder
 from nineturn.core.types import nt_layers_list
 
@@ -31,6 +31,8 @@ def FTSA(num_heads:int,  input_d:int,embed_dims:List[int], n_nodes:int, window_s
     model = None
     if time_agg == 'concate':
         model = FTSAConcate(num_heads,input_d,embed_dims,n_nodes, window_size, time_kernel, simple_decoder, **kwargs)
+    elif time_agg == 'sum':
+        model = FTSASum(num_heads,input_d,embed_dims,n_nodes, window_size, time_kernel, simple_decoder, **kwargs)
     return model
 
 
@@ -110,9 +112,20 @@ class FTSASum(SlidingWindowFamily):
         self.nn_layers = nt_layers_list()
         for emb in embed_dims:
             self.nn_layers.append(TSA(out_dim=emb, num_heads=num_heads, **kwargs))
-        self.time2vec = Time2Vec(time_kernal, **kwargs)
-        self.time_dimention = tf.range(window_size, dtype=tf.float32)
-        self.time_kernal = time_kernal
+        self.time2vec = Time2Vec(time_kernel,input_d, **kwargs)
+        self.time_dimention = tf.reshape(tf.range(window_size, dtype=tf.float32), [1,-1])
+        self.time_kernel = time_kernel
+        self.feature_size = input_d
+
+
+    def build(self, input_shape):
+        print(self.time_kernel)
+        self.wk = self.add_weight(
+            shape=(1, self.time_kernel),
+            initializer='uniform',
+            trainable=True
+        )
+
 
     def call(self, in_state: Tuple[Tensor, List[int]]):
         """Forward function."""
@@ -125,11 +138,17 @@ class FTSASum(SlidingWindowFamily):
         node_embs = tf.gather(node_embs, ids)
         self.memory.update_window(node_embs.numpy(),ids)
         input_windows = tf.convert_to_tensor(self.memory.get_memory(ids),dtype=tf.float32)  #N, W, D N serve as batch size in this case
-        time_encoding = self.time2vec(self.time_dimention)
-        input_with_time = tf.concat([input_windows, [time_encoding for i in
-            range(node_embs.shape[0])]], -1)
+        time_encoding = self.time2vec(self.time_dimention) # 1, W, D, K
+        time_encoding = tf.reshape(time_encoding, [-1, self.feature_size, self.time_kernel]) #W D K
+        input_with_time = tf.einsum('abc, bch->abch',input_windows ,time_encoding) # N W D K
+        input_with_time = tf.transpose(input_with_time, [0,1,3,2]) # N W k D
+        input_with_time = tf.reshape(input_with_time, [-1, self.window_size* self.time_kernel, self.feature_size]) 
+        # N W*K D
         for layer in self.nn_layers:
             input_with_time = layer(input_with_time,input_with_time)
+
+        input_with_time = tf.reshape(input_with_time, [node_embs.shape[0], self.window_size, self.time_kernel, -1]) 
+        input_with_time = tf.tensordot(input_with_time, tf.squeeze(self.wk), [[2],[0]])
         last_sequence = tf.slice(input_with_time,
                 [0,self.window_size-1, 0],
                 [input_with_time.shape[0], 1, input_with_time.shape[2]])
